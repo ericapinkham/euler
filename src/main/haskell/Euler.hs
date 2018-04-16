a26e49b735a90852f2922100313fb6d1d8b2b1f9
Eric Pinkham <eric.a.pinkham@gmail.com>
2018-04-16 09:29:13 -0700

    053

--- src/main/haskell/Euler.hs
+++ src/main/haskell/Euler.hs
@@ -1,7 +1,15 @@
 module Euler
-( divisors
+( divisors,
+  choose
 ) where
 
 divisors :: Int -> [Int]
 divisors n = foldl (\acc x -> if n `quot` x == x then x:acc else x:(n `quot` x):acc) [] $ filter (\x -> n `mod` x == 0) $ takeWhile (< (roundupsqrt n)) [1..]
     where roundupsqrt x = ceiling (sqrt (fromIntegral x))
+
+choose :: Integer -> Integer -> Integer
+choose n k
+  | n == k = 1
+  | k == 0 = 1
+  | k > n = error "k exceeds n"
+  | otherwise = (foldl1 (*) [(k+1)..n]) `div` (foldl1 (*) [1..(n-k)])
